9cf82b7032fc7bc3beb8cedcb4473687335e2895
Matthew Rahtz <matthew.rahtz@gmail.com>
2016-04-22 13:46:51 +0100

    Order plots

--- graph_spike_trains.py
+++ graph_spike_trains.py
@@ -6,52 +6,63 @@ import pickle
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import numpy as np
+import utils
+import os.path
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pickle_file')
 args = parser.parse_args()
+
+if 'scale' in args.pickle_file:
+    n_notes = 7
+elif '_three_' in args.pickle_file:
+    n_notes = 3
+elif '_two_' in args.pickle_file:
+    n_notes = 2
+else:
+    raise Exception("Unknown number of notes for pickle '%s'" %
+                    args.pickle_file)
+note_length = 0.5
 
 print("Loading results...")
 with open(args.pickle_file, 'rb') as pickle_file:
     monitors = pickle.load(pickle_file)
 print("Done!")
 
-def spike_range(spike_times, spike_indices, from_time, to_time):
-    max_time = np.amax(spike_times)
-    if to_time == 'end':
-        to_time = max_time
-    if from_time < 0:
-        from_time = max_time + from_time
-
-    relevant_indices = (spike_times >= from_time) & (spike_times <= to_time)
-    relevant_spike_times = spike_times[relevant_indices]
-    relevant_spike_indices = spike_indices[relevant_indices]
-    return (relevant_spike_indices, relevant_spike_times)
-
 input_spike_indices = monitors['spikes']['input']['i']
 input_spike_times = monitors['spikes']['input']['t']
-(i_start_i, i_start_t) = \
-    spike_range(input_spike_times, input_spike_indices, 0, 5)
-(i_end_i, i_end_t) = \
-    spike_range(input_spike_times, input_spike_indices, -5, 'end')
 
 output_spike_indices = monitors['spikes']['layer1e']['i']
 output_spike_times = monitors['spikes']['layer1e']['t']
-(o_start_i, o_start_t) = \
-    spike_range(output_spike_times, output_spike_indices, 0, 5)
-(o_end_i, o_end_t) = \
-    spike_range(output_spike_times, output_spike_indices, -5, 'end')
 
 plt.ion()
 plt.figure(figsize=(6,4.5))
+
+# input spikes
 plt.subplot(2, 2, 1)
-plt.plot(i_start_t, i_start_i, 'k.', markersize=2)
+plt.plot(input_spike_times, input_spike_indices, 'k.', markersize=2)
+plt.xlim([0, 5])
 plt.subplot(2, 2, 2)
-plt.plot(i_end_t, i_end_i, 'k.', markersize=2)
+plt.plot(input_spike_times, input_spike_indices, 'k.', markersize=2)
+max_time = np.amax(input_spike_times)
+plt.xlim([max_time-5, max_time])
+
+# output spikes
+favourite_notes = utils.analyse_note_responses(
+    output_spike_indices,
+    output_spike_times,
+    note_length,
+    n_notes,
+    from_time=max_time/2
+)
 plt.subplot(2, 2, 3)
-plt.plot(o_start_t, o_start_i, 'k.', markersize=2)
+utils.ordered_spike_raster(output_spike_indices, output_spike_times,
+                           favourite_notes)
+plt.xlim([0, 5])
 plt.subplot(2, 2, 4)
-plt.plot(o_end_t, o_end_i, 'k.', markersize=2)
+utils.ordered_spike_raster(output_spike_indices, output_spike_times,
+                           favourite_notes)
+plt.xlim([max_time-5, max_time])
 
 for i in [1, 2, 3, 4]:
     plt.subplot(2, 2, i)
@@ -61,9 +72,6 @@ for i in [1, 2, 3, 4]:
 for i in [1, 2]:
     plt.subplot(2, 2, i)
     plt.ylim([0, 512])
-for i in [3, 4]:
-    plt.subplot(2, 2, i)
-    plt.ylim([0, 15])
 
 plt.subplot(2, 2, 1)
 plt.ylabel("Input\nAfferent no.")
@@ -74,3 +82,7 @@ plt.subplot(2, 2, 4)
 plt.xlabel("Time (s)")
 
 plt.tight_layout(pad=0.1)
+seq_name = os.path.basename(args.pickle_file)\
+    .replace('.pickle', '').replace('monitors_', '')
+fig_name = 'input_output_spikes_%s' % seq_name
+plt.savefig('figures/%s.png' % fig_name, dpi=500)
