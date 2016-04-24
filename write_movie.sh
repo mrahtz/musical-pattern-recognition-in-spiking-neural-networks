2cf67a2cb785188231802191ceb35bb48c127e40
Matthew Rahtz <matthew.rahtz@gmail.com>
2016-04-24 20:50:54 +0100

    Reflect new location"

--- write_movie.sh
+++ write_movie.sh
@@ -8,7 +8,7 @@ AUDIO_FILE=$2
 VIDEO_FILE=$(sed 's/.pickle/.video.mp4/g' <<< "$VARS_FILE")
 OUTPUT_FILE=$(sed 's/.pickle/.mp4/g' <<< "$VARS_FILE")
 
-../write_movie.py "$VARS_FILE"
+./write_movie.py "$VARS_FILE"
 ffmpeg -y \
     -i "$VIDEO_FILE" -i "$AUDIO_FILE" \
     -vcodec h264 -shortest "$OUTPUT_FILE"
